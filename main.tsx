4b702dd2cde6500fd60ee0baa237451c3fd0558f
João Maia <joao.s.maia@tecnico.ulisboa.pt>
2024-11-10 08:54:45 +0000

    Updated TO start using shopping list as landing page

--- main.tsx
+++ main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ReactTogether } from 'react-together'
 
-import App from '@./App'
+import App from '@./Pages/ShoppingList/ShoppingListLandingPage'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ReactTogether
